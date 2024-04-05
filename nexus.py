23ccd982427d77b1524e99516e993ad218afcb26
sk9212k <sk9212k@gmail.com>
2024-04-05 10:36:46 +0100

    k17 k13 I've updated the script for nexus.py

--- nexus.py
+++ nexus.py
@@ -1,30 +1,45 @@
 import os
 import requests
 
-# Define your authentication credentials using environment variables
-username = os.getenv('AUTH_USERNAME')  # Replace 'AUTH_USERNAME' with the actual name of your environment variable
-password = os.getenv('AUTH_PASSWORD')  # Replace 'AUTH_PASSWORD' with the actual name of your environment variable
+# Get the current directory
+current_directory = os.getcwd()
 
-# Make sure both username and password are not None before proceeding
+# Define the directory path relative to the current directory
+directory_path = current_directory
+
+# Get the username and password from environment variables
+username = os.getenv('AUTH_USERNAME')
+password = os.getenv('AUTH_PASSWORD')
+
+# Check if username or password is missing
 if username is None or password is None:
     raise ValueError("Missing environment variables for authentication")
 
-les = {
+# Define the parameters
+params = (
+    ('repository', 'zap-reports'),  # Repository name
+)
+
+# Define the files to upload
+files = {
     'raw.asset1': ('zap-alerts.txt', open('zap-alerts.txt', 'rb')),  # Adjust file name and path accordingly
     'raw.asset2': ('zap-alerts-world.txt', open('zap-alerts-world.txt', 'rb')),  # Adjust file name and path accordingly
 }
+
 # Define additional data
 data = {
     'raw.directory': directory_path,  # Adjust directory path if necessary
     'raw.asset1.filename': 'zap-alerts.txt',  # Adjust file name accordingly
     'raw.asset2.filename': 'zap-alerts-world.txt',  # Adjust file name accordingly
 }
-# Make the POST request with the authentication credentials from environment variables
-response = requests.post('http://localhost:8081/service/rest/v1/components?repository=zap-reports', params=params, files=files, data=data, auth=(username, password))
+
+# Make the POST request
+response = requests.post('http://localhost:8081/service/rest/v1/components', params=params, files=files, data=data, auth=(username, password))
 
 # Check the response status code
 if response.status_code == 204:
     print("Files uploaded successfully!")
 else:
     print(f"Failed to upload files. Status code: {response.status_code}")
+
 
